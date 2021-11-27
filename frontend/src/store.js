83be1d91eef1559bb2b2474b71c64b63c31bafec
chasan <chasankiosentemir@gmail.com>
2021-11-27 04:25:52 +0200

    Implement rate and review for products

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -8,6 +8,7 @@ import {
   productUpdateReducer,
   productDeleteReducer,
   productCategoryListReducer,
+  productReviewCreateReducer,
 } from "./reducers/productReducers";
 import {
   userDetailsReducer,
@@ -68,6 +69,7 @@ const reducer = combineReducers({
   userUpdate: userUpdateReducer,
   userTopSellersList: userTopSellerListReducer,
   productCategoryList: productCategoryListReducer,
+  productReviewCreate: productReviewCreateReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
