4c5d7d03f70346da9d3c34494e83ccf9eb0a8616
chasan <chasankiosentemir@gmail.com>
2021-11-27 09:43:32 +0200

    Fix Bugs

--- backend/data.js
+++ backend/data.js
@@ -1,95 +1,98 @@
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
 
 const data = {
-
-    users: [
-        {
-            name: 'Chasan',
-            email: 'admin@example.com',
-            password: bcrypt.hashSync('1234', 8),
-            isAdmin: true,
-        },
-
-        {
-            name: 'John',
-            email: 'user@example.com',
-            password: bcrypt.hashSync('1234', 8),
-            isAdmin: false,
-        },
-    ],
-    products: [{
-        name: 'Dolce and Gabbana Trouser',
-        category: 'Trouser',
-        image: '/images/product-1.jpg',
-        price: 120,
-        countInStock:10,
-        brand: 'Dolce and Gabbana',
-        rating: 4.5,
-        numReviews: 10,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-
-    }, 
+  users: [
     {
-        name: 'Dolce and Gabbana Shirt',
-        category: 'Trouser',
-        image: '/images/product-2.jpg',
-        price: 120,
-        countInStock:20,
-        brand: 'Dolce and Gabbana',
+      name: "Chasan",
+      email: "admin@example.com",
+      password: bcrypt.hashSync("1234", 8),
+      isAdmin: true,
+      isSeller: true,
+      seller: {
+        name: "Puma",
+        logo: "/images/logo1.png",
+        description: "best seller",
         rating: 4.5,
-        numReviews: 10,
-        description:'100% Cotton'
+        numReviews: 120,
+      },
+    },
 
-    }, 
     {
-        name: 'Adidas Trouser',
-        category: 'Trouser',
-        image: '/images/product-1.jpg',
-        price: 120,
-        countInStock:0,
-        brand: 'Dolce and Gabbana',
-        rating: 4.5,
-        numReviews: 10,
-        description:'100% Cotton'
-
-    }, 
+      name: "John",
+      email: "user@example.com",
+      password: bcrypt.hashSync("1234", 8),
+      isAdmin: false,
+    },
+  ],
+  products: [
     {
-        name: 'Nike Trouser',
-        category: 'Trouser',
-        image: '/images/product-4.jpg',
-        price: 120,
-        countInStock:10,
-        brand: 'Nike',
-        rating: 4.5,
-        numReviews: 10,
-        description:'100% Cotton'
-
-    }, 
+      name: "Dolce and Gabbana Trouser",
+      category: "Trouser",
+      image: "/images/product-1.jpg",
+      price: 120,
+      countInStock: 10,
+      brand: "Dolce and Gabbana",
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
     {
-        name: 'Trussardi Trouser',
-        category: 'Shirt',
-        image: '/images/product-5.jpg',
-        price: 120,
-        countInStock:0,
-        brand: 'Dolce and Gabbana',
-        rating: 4.5,
-        numReviews: 1001,
-        description:'100% Cotton'
-
-    }, 
+      name: "Dolce and Gabbana Shirt",
+      category: "Trouser",
+      image: "/images/product-2.jpg",
+      price: 120,
+      countInStock: 20,
+      brand: "Dolce and Gabbana",
+      rating: 4.5,
+      numReviews: 10,
+      description: "100% Cotton",
+    },
     {
-        name: 'Nike Shirt',
-        category: 'Trouser',
-        image: '/images/product-6.jpg',
-        price: 0,
-        countInStock:15,
-        brand: 'Dolce and Gabbana',
-        rating: 4.5,
-        numReviews: 15,
-        description:'100% Cotton'
-
-    }, 
-   ],
+      name: "Adidas Trouser",
+      category: "Trouser",
+      image: "/images/product-1.jpg",
+      price: 120,
+      countInStock: 0,
+      brand: "Dolce and Gabbana",
+      rating: 4.5,
+      numReviews: 10,
+      description: "100% Cotton",
+    },
+    {
+      name: "Nike Trouser",
+      category: "Trouser",
+      image: "/images/product-4.jpg",
+      price: 120,
+      countInStock: 10,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "100% Cotton",
+    },
+    {
+      name: "Trussardi Trouser",
+      category: "Shirt",
+      image: "/images/product-5.jpg",
+      price: 120,
+      countInStock: 0,
+      brand: "Dolce and Gabbana",
+      rating: 4.5,
+      numReviews: 1001,
+      description: "100% Cotton",
+    },
+    {
+      name: "Nike Shirt",
+      category: "Trouser",
+      image: "/images/product-6.jpg",
+      price: 0,
+      countInStock: 15,
+      brand: "Dolce and Gabbana",
+      rating: 4.5,
+      numReviews: 15,
+      description: "100% Cotton",
+    },
+  ],
 };
 
 export default data;
